40f9a024d259fed01dd39438adb0c96decfba901
fomenko733 <nfomenko733@gmail.com>
2026-01-22 00:00:32 +0500

    lesson_01

---
+++ lesson_01/tesk_5.py
@@ -0,0 +1,14 @@
+def print_number(num):
+    print(num)
+
+print_number(8)
+print_number(9)
+print_number(1)
+print_number(9)
+print_number(4)
+print_number(6)
+print_number(8)
+print_number(0)
+print_number(5)
+print_number(9)
+print_number(1)
